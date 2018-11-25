236669024548ca4a2477bd50ed6684cfc72a838a
Vaishnavi Naik <vaishnavi.naik05@gmail.com>
2018-11-25 17:35:08 +0530

    registaton modificarion for those who dont upload image

--- reg_submit.php
+++ reg_submit.php
@@ -8,19 +8,30 @@ if(isset($_POST["submit"]))
 	$email = $_POST["email"];
 	$pass = $_POST["pass"];
 	$dept = $_POST["dept"];
-
-	$check = getimagesize($_FILES["image"]["tmp_name"]);
-  	if($check !== false  && in_array(mime_content_type ($_FILES["image"]["tmp_name"]) , array('image/jpeg','image/png','image/gif'))){
-		$image = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
-	  	$query = "INSERT INTO user(name, email, password, dept, profile_pic) VALUES ('$name', '$email', '$pass', '$dept', '$image')";  
+	if (!empty($_FILES["image"]["tmp_name"])){
+		$check = getimagesize($_FILES["image"]["tmp_name"]);
+	  	if($check !== false  && in_array(mime_content_type ($_FILES["image"]["tmp_name"]) , array('image/jpeg','image/png','image/gif'))){
+			$image = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
+		  	$query = "INSERT INTO user(name, email, password, dept, profile_pic) VALUES ('$name', '$email', '$pass', '$dept', '$image')";  
+		    if(mysqli_query($connect, $query) or die(mysqli_error($connect)))  
+		    {  
+		    	$msg = "Registration Successful!"; 
+		    }else{
+		 		$msg = "User not Registered. Try Again!";
+		    }
+		    header('location: register.php?msg=' . $msg);	  	
+		}else{
+			header('location: register.php?img="Upload an image!"');
+		}    
+	}else{
+		$query = "INSERT INTO user(name, email, password, dept) VALUES ('$name', '$email', '$pass', '$dept')";  
 	    if(mysqli_query($connect, $query) or die(mysqli_error($connect)))  
 	    {  
 	    	$msg = "Registration Successful!"; 
 	    }else{
 	 		$msg = "User not Registered. Try Again!";
 	    }
-	    header('location: register.php?msg=' . $msg);	  	
-	}else{
-		header('location: register.php?img="Upload an image!"');
-	}    
+	    header('location: register.php?msg=' . $msg);
+	}
+	
 }  
