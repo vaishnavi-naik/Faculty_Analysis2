58b7f22bde07e3b328549efcacc888b7c4bae9e3
SIDDHUMITHRA <42208972+SIDDHUMITHRA@users.noreply.github.com>
2018-11-28 19:14:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,9 +28,9 @@
             <h1 style="text-shadow:3px 3px 5px black;margin-top:-75%;color:#c90219;font-size:60px;">WELCOME TO FACULTY ANALYSIS</h1>
             <p></p>
             <?php if(isset($_SESSION['email'])){?>
-              <p><a class="btn btn-lg btn-primary" href="dashboard.php" style="width:auto;margin-top: 250px;" role="button">GO TO DASHBOARD</a></p>
+              <p><a class="btn btn-lg btn-primary" href="dashboard.php" style="width:auto;margin-top: 230px;" role="button">GO TO DASHBOARD</a></p>
             <?php }else{?>
-            <p><a class="btn btn-lg btn-primary" href="login.php" style="width:auto;margin-top: 90px;" role="button">LOGIN</a></p>
+            <p><a class="btn btn-lg btn-primary" href="login.php" style="width:auto;margin-top: 230px;" role="button">LOGIN</a></p>
             <?php }?>
           </div>
         </div>
