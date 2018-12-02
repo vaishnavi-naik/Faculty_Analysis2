c259df2edd8e65d630bd39e1904107c272e60a29
SIDDHUMITHRA <42208972+SIDDHUMITHRA@users.noreply.github.com>
2018-12-02 12:08:57 +0530

    Update dashboard_new.php

--- dashboard_new.php
+++ dashboard_new.php
@@ -218,6 +218,7 @@ if((mysqli_query($connect, $query) ) or die(mysqli_error($connect)))
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
+    
         <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
